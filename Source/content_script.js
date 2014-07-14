96a72812c0ddc9c438d3818c365714769cc795d0
Doug Tabacco <tabacco@dougtabacco.com>
2014-07-14 12:18:08 -0700

    Convert Cloud to Butt to It's Video Games

--- Source/content_script.js
+++ Source/content_script.js
@@ -30,12 +30,7 @@ function walk(node)
 function handleText(textNode) 
 {
 	var v = textNode.nodeValue;
-
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\b([Vv])ideogame(s?)\b/g, "$1ideo game$2");
 	textNode.nodeValue = v;
 }
 
